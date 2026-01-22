f0174dc1fd8cdec6f003a710e5ca6f82e3985c35
uni.kit <uni.ai.consalting@gmail.com>
2026-01-22 10:27:00 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "UNI-Landing"; // имя репозитория
+
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
