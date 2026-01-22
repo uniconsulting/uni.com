330019e5c5a6d0089075febafcd3dadc8083f1ef
uni.kit <uni.ai.consalting@gmail.com>
2026-01-22 12:28:26 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
+export default {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
