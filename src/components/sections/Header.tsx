d978b01aabf2c8394583fca5575aa33befde8b16
uni.kit <uni.ai.consalting@gmail.com>
2026-01-22 21:19:44 +0400

    Update Header.tsx

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useEffect, useMemo, useState } from "react";
+import { PhoneIcon } from "@/components/icons";
 
 type NavItem = { label: string; href: string; chevron?: boolean };
 
@@ -31,20 +32,8 @@ function ChevronDown(props: React.SVGProps<SVGSVGElement>) {
   );
 }
 
-function PhoneIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" {...props}>
-      <path
-        d="M8.7 10.3c1.3 2.5 3.5 4.7 6 6l1.9-1.9c.3-.3.8-.4 1.2-.2 1.1.4 2.3.7 3.6.8.6.1 1 .6 1 1.2V20c0 .7-.6 1.2-1.3 1.2C11 21.2 2.8 13 2.8 2.9 2.8 2.2 3.4 1.6 4.1 1.6H7c.6 0 1.1.4 1.2 1 .1 1.2.4 2.5.8 3.6.1.4 0 .9-.3 1.2L6.9 9.2"
-        stroke="currentColor"
-        strokeWidth="1.6"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
-
+<PhoneIcon className="h-5 w-5 text-white/80" />
+  
 function TelegramIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 24 24" fill="none" {...props}>
