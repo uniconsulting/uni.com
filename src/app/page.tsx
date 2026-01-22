59e1940d0584ebcb16ceadc71b00f88a162a7aa0
uni.kit <uni.ai.consalting@gmail.com>
2026-01-22 19:43:36 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/sections/Header";
 import Hero from "../components/sections/Hero";
+import CTA1 from "../components/sections/CTA1";
 
 export default function Page() {
   return (
     <>
       <Header />
+      <CTA1 />
       <main>
         <Hero />
       </main>
