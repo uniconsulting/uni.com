e0927bfa0ed3c79b82d3f3d696d384fa7a82bd38
uni.kit <uni.ai.consalting@gmail.com>
2026-01-22 12:04:00 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,13 @@
 import type { NextConfig } from "next";
 
-const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
-const basePath = isGitHubActions && repo ? `/${repo}` : "";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
-
-  basePath,
-  assetPrefix: basePath,
-
-  // Чтобы CSS мог подставить префикс для public-ассетов
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
+  basePath: isProd ? "/uni.com" : "",
+  assetPrefix: isProd ? "/uni.com/" : "",
 };
 
 export default nextConfig;
