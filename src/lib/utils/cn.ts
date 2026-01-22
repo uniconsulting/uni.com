b198acddb83c687d1ce14bf5e31d0826bab7a585
uni.kit <uni.ai.consalting@gmail.com>
2026-01-22 10:06:30 +0400

    Update cn.ts

--- src/lib/utils/cn.ts
+++ src/lib/utils/cn.ts
@@ -1,2 +1,6 @@
-export const cn = (...classes: Array<string | undefined>) =>
-  classes.filter(Boolean).join(" ");
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
