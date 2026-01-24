c053ddccbb2355c8817de7f225cede607bff4540
uni.kit <uni.ai.consalting@gmail.com>
2026-01-24 14:52:09 +0400

    Update Plans.tsx

--- src/components/sections/Plans.tsx
+++ src/components/sections/Plans.tsx
@@ -539,7 +539,7 @@ export default function PricingPlansSection() {
 
                       <div className="mt-4">
                         <div className="flex items-end gap-2">
-                          <div className="text-[26px] font-semibold text-[#0f172a] tracking-[-0.02em]">
+                          <div className="text-[26px] font-semibold text-[#41e18b] tracking-[-0.02em]">
                             {priceText}
                           </div>
 
